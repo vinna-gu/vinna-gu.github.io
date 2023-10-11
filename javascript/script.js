eaa5e53af6b04f82a76c8aa024644996d3d7813f
Vinna Gu <vinnagu@Vinnas-MBP-2.lan>
2023-10-11 08:54:21 -0400

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
+
+	// Make sure this.hash has a value before overriding default behavior
+	if (this.hash !== "") {
+		// Prevent default anchor click behavior
+		event.preventDefault();
+		// Store hash
+		var hash = this.hash;
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+			  scrollTop: $(hash).offset().top
+			}, 800, function(){
+		   
+			    // Add hash (#) to URL when done scrolling (default click behavior)
+			    window.location.hash = hash;
+		    });
+		} // End if
+	});
+});
+
+var windw = this;
+
+$.fn.followTo = function ( pos ) {
+    var $this = this,
+        $window = $(windw);
+    
+    $window.scroll(function(e){
+        if ($window.scrollTop() > pos) {
+            $this.css({
+                position: 'absolute',
+                top: pos
+            });
+        } else {
+            $this.css({
+                position: 'fixed',
+                top: 0
+            });
+        }
+    });
+};
+
+$('#f').followTo(250);
